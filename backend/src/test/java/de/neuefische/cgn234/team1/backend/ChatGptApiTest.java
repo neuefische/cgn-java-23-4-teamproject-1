6dd88d6df419cacdf4d2966050b77051b2ef1642
Sviat <slsvyatko@gmail.com>
2024-01-29 10:45:55 +0100

    added integration and unit tests for chatGptBackend

--- backend/src/test/java/de/neuefische/cgn234/team1/backend/ChatGptApiTest.java
+++ backend/src/test/java/de/neuefische/cgn234/team1/backend/ChatGptApiTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,27 +41,83 @@ class ChatGptApiTest {
     }
 
     @DynamicPropertySource
-    static void backendProperties(DynamicPropertyRegistry registry) {
-        registry.add("api.url", () -> mockWebServer.url("/").toString());
+    public static void configureUrl(DynamicPropertyRegistry registry) {
+        registry.add("app.chatgpt.api.url", () -> mockWebServer.url("/").toString());
+        registry.add("app.openai.api.key", () -> mockWebServer.url("/").toString());
+        registry.add("app.openai.api.org", () -> mockWebServer.url("/").toString());
     }
 
     @Test
+    @DirtiesContext
     void testApiCall() throws Exception {
         mockWebServer.enqueue(new MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .setBody("""
                         {
-                            "title": "Make a training"
+                            "id": "chatcmpl-8fZhl72ZsoE0zPwYE5GUulCbB35Ch",
+                            "object": "chat.completion",
+                            "created": 1704919389,
+                            "model": "gpt-3.5-turbo-0613",
+                            "choices": [
+                                {
+                                    "index": 0,
+                                            "message": {
+                                                "role": "assistant",
+                                                "content": "Beine sind wichtig!"
+                                            },
+                                    "logprobs": null,
+                                    "finish_reason": "stop"
+                                }
+                            ],
+                            "usage": {
+                                "prompt_tokens": 13,
+                                "completion_tokens": 5,
+                                "total_tokens": 18
+                            },
+                            "system_fingerprint": null
                         }
-                    """)
-                .addHeader("Content-Type", "application/json"));
+                         """));
+
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .setBody("""
+                        {
+                            "id": "chatcmpl-8fZhl72ZsoE0zPwYE5GUulCbB35Ch",
+                            "object": "chat.completion",
+                            "created": 1704919389,
+                            "model": "gpt-3.5-turbo-0613",
+                            "choices": [
+                                {
+                                    "index": 0,
+                                            "message": {
+                                                "role": "assistant",
+                                                "content": "Gute Beine Training"
+                                            },
+                                    "logprobs": null,
+                                    "finish_reason": "stop"
+                                }
+                            ],
+                            "usage": {
+                                "prompt_tokens": 13,
+                                "completion_tokens": 5,
+                                "total_tokens": 18
+                            },
+                            "system_fingerprint": null
+                        }
+                         """));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/workouts/generate")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                                     {
-                                        "title": "Make a training"
-                                     }
-                                """))
+                        .content(
+                                """
+                                                {
+                                                    "title": "Beine Training"
+                                                }
+                                """
+                        ))
+
                 .andExpect(status().isOk());
     }
+
+
 }
--- backend/src/test/java/de/neuefische/cgn234/team1/backend/IntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgn234/team1/backend/IntegrationTest.java
@@ -3,16 +3,23 @@ package de.neuefische.cgn234.team1.backend;
 import de.neuefische.cgn234.team1.backend.model.Workout;
 import de.neuefische.cgn234.team1.backend.repo.WorkoutRepository;
 import de.neuefische.cgn234.team1.backend.service.ChatGptGenerateService;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -30,6 +37,27 @@ class IntegrationTest {
 
     @Autowired
     private ChatGptGenerateService chatGptGenerateService = mock(ChatGptGenerateService.class);
+
+    private static MockWebServer mockWebServer;
+
+    @DynamicPropertySource
+    public static void configureUrl(DynamicPropertyRegistry registry) {
+        registry.add("app.chatgpt.api.url", () -> mockWebServer.url("/").toString());
+        registry.add("app.openai.api.key", () -> mockWebServer.url("/").toString());
+        registry.add("app.openai.api.org", () -> mockWebServer.url("/").toString());
+    }
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
 
     @Test
     @DirtiesContext
