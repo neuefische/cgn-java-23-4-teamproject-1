2b5a5abcaf326e05400ab1d60d60f9d3c4d2ada0
kahlamari <email@vincent-kahl.net>
2024-01-30 10:55:51 +0100

    fix sonar issue (WIP)

--- backend/src/main/java/de/neuefische/cgn234/team1/backend/service/CloudinaryService.java
+++ backend/src/main/java/de/neuefische/cgn234/team1/backend/service/CloudinaryService.java
@@ -1,14 +1,16 @@
 package de.neuefische.cgn234.team1.backend.service;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class CloudinaryService {
@@ -18,15 +20,27 @@ public class CloudinaryService {
         this.cloudinary = cloudinary;
     }
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        File fileToUpload = File.createTempFile("file", null);
+    public static String generateRandomFileName(String originalFileName) {
+        String fileExtension = "";
+        int i = originalFileName.lastIndexOf('.');
+        if (i > 0) {
+            fileExtension = originalFileName.substring(i + 1);
+        }
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString() + (fileExtension.isEmpty() ? "" : "." + fileExtension);
+    }
+
+    public String uploadFile(@NonNull MultipartFile file) throws IOException {
+        String randomFileName = generateRandomFileName(Objects.requireNonNull(file.getOriginalFilename()));
+        Path fileToUpload = Files.createTempFile("file", null);
         file.transferTo(fileToUpload);
         var cloudinaryResponse = cloudinary.uploader().upload(fileToUpload, Map.of(
                 "resource_type", "auto",
-                "public_id", Objects.requireNonNull(fileToUpload.getName()) + Instant.now(),
+                "public_id", randomFileName,
                 "folder", "cloudinary_file_test"
         ));
 
         return cloudinaryResponse.get("secure_url").toString();
     }
+
 }
