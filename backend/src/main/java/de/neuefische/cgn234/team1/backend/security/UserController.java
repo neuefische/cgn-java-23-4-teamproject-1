6a40d7d506e1282cefd61d4fc90fdd1edb3e3afb
kahlamari <email@vincent-kahl.net>
2024-01-31 15:08:51 +0100

    get the username with 2 methods

--- backend/src/main/java/de/neuefische/cgn234/team1/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/cgn234/team1/backend/security/UserController.java
@@ -2,18 +2,30 @@ package de.neuefische.cgn234.team1.backend.security;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("me")
-    public String getCurrentUser(@AuthenticationPrincipal Principal principal) {
-        return SecurityContextHolder.getContext().getAuthentication().getName();
+    public String getCurrentUser(@AuthenticationPrincipal OAuth2User principal) {
+        if (principal == null) {
+            return null;
+        }
+
+        Object principalObject = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principalObject instanceof OAuth2User) {
+            OAuth2User oAuth2User = (OAuth2User) principalObject;
+
+            System.out.println("Secruity: " + oAuth2User.getAttribute("login"));
+            System.out.println("Principal: " + principal.getAttribute("login"));
+        }
+
+        return principal.getAttribute("login");
     }
 }
